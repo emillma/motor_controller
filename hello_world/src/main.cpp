88703dc2a9a645f098e7b6e3db28d329ce9f3902
Emil Martens <emil.martens@gmail.com>
2023-09-19 16:53:13 +0000

    started on 2 core setup

--- hello_world/src/main.cpp
+++ hello_world/src/main.cpp
@@ -5,6 +5,8 @@
 
 #include "pico/stdlib.h"
 #include "pico/stdio.h"
+#include "pico/util/queue.h"
+#include "pico/multicore.h"
 #include "hardware/watchdog.h"
 
 #include "interface_usb.hpp"
@@ -14,6 +16,26 @@
 #include "stim_uart.hpp"
 #include "f9p_i2c.hpp"
 
+queue_t ab_queue;
+queue_t ba_queue;
+
+typedef struct
+{
+    uint8_t data[1024];
+    int32_t len;
+} f9p_message_t;
+
+f9p_message_t f9p_slots[8];
+
+__attribute__((aligned(32))) 
+static uint8_t stim_buffer[2][STIM_BUFFER_SIZE];
+
+void core1_entry()
+{
+
+
+}
+
 int main()
 {
     stdio_init_all();
@@ -22,23 +44,26 @@ int main()
     gpio_set_dir(25, GPIO_OUT);
     blink_fast();
 
-    __attribute__((aligned(32))) static uint8_t stim_buffer[2][STIM_BUFFER_SIZE];
+    queue_init(&ab_queue, sizeof(f9p_message_t), 8);
+    queue_init(&ba_queue, sizeof(f9p_message_t), 8);
 
-    size_t stim_buffer_size = sizeof(stim_buffer);
-
+    // for (int i =0; i<8;i++)
+    //     queue_add_blocking(&ab_queue, &f9p_slots[i]);
+        
     usb_init();
-    stim_init(stim_buffer);
+    // stim_init(stim_buffer);
     pwm_init();
     pio_inverter_init();
     i2c_init();
 
+    multicore_launch_core1(core1_entry);
     watchdog_enable(5000, 1);
+
     while (true)
     {
         watchdog_update();
-        stim_forward(stim_buffer);
-        f9p_a_forward();
-        f9p_b_forward();
+        // stim_forward(stim_buffer);
+
         // sleep_ms(500);
     }
 }
